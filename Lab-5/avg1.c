d2bb669e10e4c691c2ab5525d9a98d9f592f27e6
Palak <145643268+palakkagathara@users.noreply.github.com>
2023-09-28 19:39:45 +0530

    Delete Lab-5 directory

--- Lab-5/avg1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-	int a = 10;
-	int b = 20;
-	int c = 30;
-	float avg = (a+b+c)/3;
-	printf("Avg : %f",avg);
-}
--- Lab-5/sum1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-	int a,b,sum;
-	a = 10;
-	b = 20;
-	sum = a+b;
-	printf("sum : %d",sum);
-}
